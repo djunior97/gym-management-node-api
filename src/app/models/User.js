82bcdf4674b9669bd79eca4c6560c6be1f09a2eb
dirceujr <djjunior1997@hotmail.com>
2019-12-19 08:32:00 -0300

    added hook before save user to encrypt password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -14,6 +14,18 @@ class User extends Model {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
