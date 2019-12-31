8fe80b7dcddc57fb976a5664489e858d4d0f7adb
dirceujr <dirceu.antunes@garupa.co>
2019-12-31 10:02:53 -0300

    added student model initialization

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import 'dotenv/config';
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
+import Student from '../app/models/Student';
 
-const models = [User];
+const models = [User, Student];
 
 class Database {
   constructor() {
